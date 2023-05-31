7951e40225923fa20daea1a727131e08d48d21e1
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-05-31 11:49:46 +0300

    "20.5.23 commit"

--- Tests/Vision/circledetection.py
+++ Tests/Vision/circledetection.py
@@ -1,38 +1,19 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot
+
 
 cam = cv2.VideoCapture(0)
 cam.set(3,1280)
 cam.set(4,720)
 cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
-#
-# aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
-#
-# # Create the ArUco parameters
-# aruco_params = cv2.aruco.DetectorParameters_create()
 
-# Load the image
-# image = cv2.imread("image.jpg")
 
-# Convert the image to grayscale
-# gray = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
-
-# Detect the ArUco markers
-# corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
 while cam.isOpened():
     aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
 
     # Create the ArUco parameters
     aruco_params = cv2.aruco.DetectorParameters_create()
 
-    # Load the image
-    # image = cv2.imread("image.jpg")
-
-    # Convert the image to grayscale
-    # gray = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
-
-    # Detect the ArUco markers
 
     # Capturing each frame of our video stream
     ret, Img = cam.read()
@@ -62,31 +43,69 @@ while cam.isOpened():
                     dx = circle_center[0] - aruco_center[0]
                     dy = circle_center[1] - aruco_center[1]
                     angle = np.degrees(np.arctan2(dy, dx))
-
+                    axis_x = (circle_center[0]-50,circle_center[1])
+                    axis_y = (circle_center[0], circle_center[1] -50)
+                    cv2.arrowedLine(
+                        Img,
+                        tuple(circle_center),
+                        axis_x,
+                        (0, 0, 0),  # Red color
+                        2,  # Thickness of the arrowed line
+                        tipLength=0.2
+                    )
+                    cv2.arrowedLine(
+                        Img,
+                        tuple(circle_center),
+                        axis_y,
+                        (0, 0, 0),  # Red color
+                        2,  # Thickness of the arrowed line
+                        tipLength=0.2
+                    )
                     print('the last ids that detected is:',ids[-1],'and the first orientation between him and the camera is', angle)
 
                     if ids[-1] == 43 : #define that 43 is the head of the arrow orientation
-                        # dx = circle_center[0] - aruco_center[0]
-                        # dy = circle_center[1] - aruco_center[1]
-                        dx_43 = circle_center[0] - aruco_center[0]
-                        dy_43 = circle_center[1] - aruco_center[1]
                         angle = np.degrees(np.arctan2(dy, dx))
+
+                        cv2.arrowedLine(
+                            Img,
+                            tuple(circle_center),
+                            tuple(aruco_center),
+                            (0, 0, 255),  # Red color
+                            2,  # Thickness of the arrowed line
+                            tipLength=0.2
+                        )
+
                         # matplotlib.pyplot.arrow(circle_center[0], circle_center[1], dx_43, dy_43, **kwargs) #to do - fill the arrow
                     elif ids[-1] == 44:
                         # dx = circle_center[0] - aruco_center[0]
                         # dy = circle_center[1] - aruco_center[1]
                         angle += 180
+                        # arrow_head_y = (round(aruco_center[0]), round(aruco_center[1] - 110))
+                        # cv2.arrowedLine(
+                        #     Img,
+                        #     tuple(circle_center),
+                        #     arrow_head_y,
+                        #     (0, 0, 255),  # Red color
+                        #     3,  # Thickness of the arrowed line
+                        #     tipLength=0.2
+                        # )
                         # angle = np.degrees(np.arctan2(dy, dx)) +180
                     elif ids[-1] == 45:
                         # dx = circle_center[0] - aruco_center[0]
                         # dy = circle_center[1] - aruco_center[1]
                         angle += 90
-                        # angle = np.degrees(np.arctan2(dy, dx)) +90
+                        # arrow_head_y = (round(aruco_center[0]), round(aruco_center[1]))
+                        # cv2.arrowedLine(
+                        #     Img,
+                        #     tuple(circle_center),
+                        #     arrow_head_y,
+                        #     (0, 0, 255),  # Red color
+                        #     3,  # Thickness of the arrowed line
+                        #     tipLength=0.2
+                        # )
                     elif ids[-1] == 46:
-                        # dx = circle_center[0] - aruco_center[0]
-                        # dy = circle_center[1] - aruco_center[1]
                         angle -= 90
-                        # angle = np.degrees(np.arctan2(dy, dx)) - 90
+
                     if angle < 0:
                         angle += 360
                     print('the orientation is:', angle)
@@ -101,6 +120,10 @@ while cam.isOpened():
                 cv2.rectangle(Img, (x - 1, y - 1), (x + 1, y + 1), (0, 255, 0), -1) #thickness of -1 means to fill the rectangle
         cv2.imshow('QueryImage', Img)
         cv2.waitKey(1) #shows continuous live video
+
+        # plt.imshow(cv2.cvtColor(Img, cv2.COLOR_BGR2RGB))
+        # plt.axis('off')
+        # plt.show()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Interupt by user')
             break
