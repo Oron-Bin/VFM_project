90a50b1d23424a1e58cf58a4de12052074bebacf
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-06-04 14:34:13 +0300

    '4.6 first commit - steps to yaw code is the code that need to be modified in card algo'

--- Tests/Vision/Aruco/steps_to_yaw.py
+++ Tests/Vision/Aruco/steps_to_yaw.py
@@ -1,157 +1,149 @@
 import numpy as np
-from math import *
-import matplotlib.pyplot as plt
-import math
-from matplotlib.path import Path
-import matplotlib.patches as patches
-import os
+import cv2
 import time
-import sys
-import random
-sys.path.insert(1,r'C:\Users\USER\Desktop\Card Control')
-
-# from package import *
-from Utils.Control.cardalgo import *
-
-initial_flag = 0
+import matplotlib.pyplot as plt
 
 
-"""This Code is use to record a video"""
+def detect_circle_info(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_blurred = cv2.blur(gray, (8, 8))
+    circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1.5, 1000, minRadius=50, maxRadius=300)
 
-filename = 'video.avi'
-frames_per_second = 60
-res = '720p'
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            circle_center = (x, y)
+            circle_radius = r
 
+            return circle_center, circle_radius
 
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
-
-# Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
-    "480p": (640, 480),
-    "720p": (1280, 720),
-    "1080p": (1920, 1080),
-    "4k": (3840, 2160),
-}
+    return None, None
 
 
-# grab resolution dimensions and set video capture to it.
-def get_dims(cap, res='1080p'):
-    width, height = STD_DIMENSIONS["480p"]
-    if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
-    ## change the current caputre device
-    ## to the resulting resolution
-    change_res(cap, width, height)
-    return width, height
+def detect_aruco_centers(frame):
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+    aruco_params = cv2.aruco.DetectorParameters_create()
 
-# Video Encoding, might require additional installs
-# Types of Codes: http://www.fourcc.org/codecs.php
-VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-}
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
 
-def get_video_type(filename):
-    filename, ext = os.path.splitext(filename)
-    if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
-    return VIDEO_TYPE['avi']
+    aruco_centers = []
+    if ids is not None:
+        for i in range(len(ids)):
+            aruco_center = np.mean(corners[i][0], axis=0)
+            aruco_centers.append(aruco_center)
+    # print(ids,aruco_centers)
+    return aruco_centers ,ids
 
 
-"""This part resposible for the close loop control using CV2 circle detection"""
+def calculate_angle(circle_center, aruco_center):
+    dx = circle_center[0] - aruco_center[0]
+    dy = circle_center[1] - aruco_center[1]
+    angle = np.degrees(np.arctan2(dy, dx))
+
+    return angle
+
+
+def display_image(image, circle_center, circle_radius):
+    if circle_center is not None and circle_radius is not None:
+        cv2.circle(image, circle_center, circle_radius, (0, 255, 0), 4)
+        cv2.circle(image, circle_center, 3, (0, 0, 255), -1)
+    cv2.imshow('QueryImage', image)
+    cv2.waitKey(1)
+
+def ids_to_angle(ids,circle_center):
+    # aruco_centers, ids = detect_aruco_centers(img)
+    # circle_center, circle_radius = detect_circle_info(img)
+    last_aruco_center = aruco_centers[-1]
+    angle = calculate_angle(circle_center, last_aruco_center)
+    if ids[-1] == 43:
+        # angle = calculate_angle(circle_center, last_aruco_center)
+        angle = angle
+        cv2.arrowedLine(
+            img,
+            tuple(circle_center),
+            tuple(last_aruco_center),
+            (0, 0, 255),
+            2,
+            tipLength=0.2
+        )
+    elif ids[-1] == 44:
+        angle += 180
+        # angle = calculate_angle(circle_center, last_aruco_center) + 180
+    elif ids[-1] == 45:
+        angle += 90
+        # angle = calculate_angle(circle_center, last_aruco_center) +90
+    elif ids[-1] == 46:
+        angle -= 90
+        # angle = calculate_angle(circle_center, last_aruco_center) -90
+    if angle < 0:
+        angle += 360
+    print('The orientation is:', angle)
+    return angle
+def plot_angles(frame_numbers, angles):
+    plt.plot(frame_numbers, angles)
+    plt.xlabel('Frame')
+    plt.ylabel('Angle')
+    plt.ylim([0, 360])
+    plt.title('Angle vs. Frame')
+    plt.show()
+
+
+# Main program
 
 cam = cv2.VideoCapture(0)
-out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cam, res))
-cam.set(3,1280)
-cam.set(4,720)
-cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
-set_des = 0
-mycard = Card(0,0,-1,-1,-1,-1,115200,'/dev/ttyACM0')
-mycard.set_motor_angle(0.0001) ## it was 0.0001
-mycard.send_data('motor')
-algo = card_algorithms(0,0)
-# while(1):
-#     mycard.set_encoder_angle(algo.output_calibrate()) ## algo.output_calibrate()
-#     mycard.send_data('encoder')
+cam.set(3, 1280)
+cam.set(4, 720)
+cam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
-
-flag = 0
+start_time = time.time()
+times = []
+angles = []
+frame_numbers = []
 
 while cam.isOpened():
-    center, Img = algo.filter_camera(cam, 3)
-    aruco = algo.find_Aruco(Img)
-    algo.finger_position(Img)
-    # Capturing each frame of our video stream
-    if set_des == 0: ## If user didnt input a value yeti
-        algo.position_user_input(Img)
-        set_des = 1
-    elif center is not None and set_des == 1: ## If the first card_center is not updated yet
-        algo.card_initialize(center[:-1])
-        set_des = 2
-        mycard.vibrate_on()
-    elif center is not None:
-        algo.desired_position(Img)
-        # print(center)
-        # print(center.tolist())
-        algo.update(center.tolist())
-        algo.plot_path(Img)
-        if algo.check_distance(5) is not True and set_des == 2:
-            output = algo.law_1()
-            mycard.set_encoder_angle(output)
-            algo.plot_arrow(Img)
-            mycard.send_data('encoder')
-        elif algo.check_distance(5) is True:
-            for i in range(10):
-                mycard.send_data('vibrate')
-                set_des = 3
-        if set_des == 3:
-            time.sleep(1)
+    ret, img = cam.read()
 
-            for i in range(10):
-                mycard.send_data('st')
+    if ret:
+        circle_center, circle_radius = detect_circle_info(img)
+        aruco_centers, ids = detect_aruco_centers(img)
+        axis_x = circle_center[0] - 50, circle_center[1]
+        axis_y = circle_center[0], circle_center[1] - 50
+        cv2.arrowedLine(
+            img,
+            tuple(circle_center),
+            axis_x,
+            (0, 0, 0),
+            2,
+            tipLength=0.2
+        )
+        cv2.arrowedLine(
+            img,
+            tuple(circle_center),
+            axis_y,
+            (0, 0, 0),
+            2,
+            tipLength=0.2
+        )
 
-            time.sleep(1)
-            algo.next_iteration()
-            algo.package_data()
+        if circle_center is not None and aruco_centers:
+            angle = ids_to_angle(ids, circle_center)
+            angles.append(angle)
+            current_time = time.time() - start_time
+            times.append(current_time)
+            frame_numbers.append(len(angles))
 
-            ## if we want throw center of mass
+        display_image(img, circle_center, circle_radius)
 
-            if flag == 1:
-                algo.random_input()
-                flag = 0
-            else:
-                algo.y_d = 312
-                algo.x_d = 624
-                flag = 1
-
-            ## if we want not threw center of mass
-
-            # algo.random_input()
-            algo.clear()
-            if algo.iteration == 10:
-                algo.export_data()
-                break
-
-            set_des = 2
-        # time.sleep(0.1)
-        algo.draw_circle(Img, center)
-        # print(output)
-    out.write(Img)
-    cv2.imshow('QueryImage', Img)
-
-
-
-    # Exit at the end of the video on the 'q' keypress
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print('Interupt by user')
+        print('Interrupted by user')
         break
-    if cv2.waitKey(1) & 0xFF == ord('i'):
-        algo.position_user_input(Img)
-#
-# cv2.destroyAllWindows()
-# plt.show()
+
+    current_time = time.time() - start_time
+    if current_time >= 20:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
+plot_angles(frame_numbers, angles)
