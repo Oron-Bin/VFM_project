508e718b36588060c5010e012941d293ff2be5c8
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-06-26 18:38:55 +0300

    '26.6 last commit - differntfunc - the model optimization code'

--- Tests/Differentfunc.py
+++ Tests/Differentfunc.py
@@ -3,11 +3,12 @@ import pandas as pd
 from scipy.optimize import least_squares
 import scipy as scipy
 
+# F_c,tau_f, f_g = 0 , 0 , [0,0]
 # Define the physical equation
 def equation(parameters, x, y, teta):
     a, c, d ,e  = parameters
     M = 14 *(10**-3) #kg
-    F_c,tau_f, f_g_X, f_g_y = a ,c, d, e
+
     return (1/M*((x**2 + y**2))) * ((a * x * np.sin(teta)) - (y * np.cos(teta)) - (x * e - y * d) + c)
 
 # Define the objective/loss function
@@ -31,10 +32,9 @@ result = least_squares(objective, initial_parameters)
 
 # Retrieve the optimized parameter values
 optimized_parameters = result.x
-
-# Analyze the results
+F_c,tau_f, f_g = optimized_parameters[0], optimized_parameters[1], [optimized_parameters[2],optimized_parameters[3]]# Analyze the results
 # print("Optimized parameters:", optimized_parameters)
 
-print('VIBRATION_FORCE:',optimized_parameters[0],'N')
-print('gravitaion_force:',np.array(optimized_parameters[2],optimized_parameters[3]),'N')
-print('torsional friction:',optimized_parameters[1],'Nm')
+print('VIBRATION_FORCE:',F_c,'N')
+print('gravitaion_force:',f_g,'N')
+print('torsional friction:',tau_f,'Nm')
