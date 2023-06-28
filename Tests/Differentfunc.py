70cf7d338df20a328611a390b2134e7d6a1acf6e
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-06-28 14:57:21 +0300

    '28.6 first commit - differntfunc - work with csv data'

--- Tests/Differentfunc.py
+++ Tests/Differentfunc.py
@@ -1,40 +1,58 @@
-import numpy as np
 import pandas as pd
-from scipy.optimize import least_squares
-import scipy as scipy
 
-# F_c,tau_f, f_g = 0 , 0 , [0,0]
-# Define the physical equation
-def equation(parameters, x, y, teta):
-    a, c, d ,e  = parameters
-    M = 14 *(10**-3) #kg
 
-    return (1/M*((x**2 + y**2))) * ((a * x * np.sin(teta)) - (y * np.cos(teta)) - (x * e - y * d) + c)
+def shortest_way(num_1, num_2):
+    """Function for finding the shortest motor path to destanation"""
 
-# Define the objective/loss function
-def objective(parameters):
-    predicted_output = equation(parameters, x_data, y_data, teta_data)
-    error = predicted_output - output_data
-    return error
+    if abs(num_1 - num_2) < 180:
+        return num_2 - num_1
+    else:
+        if num_1 > num_2:
+            return abs(num_1 - num_2 - 360)
+        else:
+            return abs(num_1 - num_2) - 360
 
-# Read data from CSV file
+
 data = pd.read_csv('/home/roblab20/Desktop/videos/data_oron/data_oron_2023-06-26-15-57-28.csv')
-x_data = data['Pos_x'].values
-y_data = data['Pos_y'].values
-teta_data = data['Motor angle'].values
-output_data = data['Orientation'].values
+pixel_factor = 1/2000
 
-# Initial guess for the parameters
-initial_parameters = np.random.rand(4)
-# print(initial_parameters)
-# Run the optimization
-result = least_squares(objective, initial_parameters)
+data.at[0, 'x_dot'] = 0
+data.at[0, 'y_dot'] = 0
+data.at[0, 'phi_dot'] = 0
+data.at[0, 'Pos_x'] = 0
+data.at[0, 'Pos_y'] = 0
 
-# Retrieve the optimized parameter values
-optimized_parameters = result.x
-F_c,tau_f, f_g = optimized_parameters[0], optimized_parameters[1], [optimized_parameters[2],optimized_parameters[3]]# Analyze the results
-# print("Optimized parameters:", optimized_parameters)
+data.at[len(data['Orientation'] -1 ), 'phi_dot'] = 0
+data.at[len(data['Orientation'] -1 ), 'x_dot'] = 0
+data.at[len(data['Orientation'] -1 ), 'y_dot'] = 0
 
-print('VIBRATION_FORCE:',F_c,'N')
-print('gravitaion_force:',f_g,'N')
-print('torsional friction:',tau_f,'Nm')
+# data.at[0, 'Motor'] = 0
+data['Pos_x'] = data['Pos_x'] * pixel_factor
+data['Pos_y'] = data['Pos_y'] * pixel_factor
+
+
+# data.at[-1, 'phi_dot'] = 0
+# data.at[-1, 'x_dot'] = 0
+# data.at[-1, 'y_dot'] = 0
+
+for i in range(1,len(data['Orientation']) -1):
+    sub_phi = shortest_way(data['Orientation'][i],data['Orientation'][i-1])
+    sub_x = (data['Pos_x'][i] - data['Pos_x'][i - 1])
+    sub_y = (data['Pos_y'][i] - data['Pos_y'][i - 1])
+    sub_time = data['Time'][i] - data['Time'][i-1]
+    char_phi = sub_phi/sub_time
+    char_x = sub_x / sub_time
+    char_y = sub_y / sub_time
+    data.at[i, 'phi_dot'] = char_phi
+    data.at[i, 'x_dot'] = char_x
+    data.at[i, 'y_dot'] = char_y
+    data.at[i,'delta_teta'] = data.at[i+1, 'delta_teta']
+
+
+
+data.to_csv('/home/roblab20/Desktop/oron.csv', index=False)
+
+
+
+
+
