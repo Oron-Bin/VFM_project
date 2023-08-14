00c4ebf539b4449030a13da923f651b9b30b153d
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-08-14 15:28:53 +0300

    '14.8 last commit - stanely control. will maybe help me after i optimize the system

--- Tests/Vision/Aruco/optimize_Parameters.py
+++ Tests/Vision/Aruco/optimize_Parameters.py
@@ -7,8 +7,8 @@ import csv
 
 
 # filename = '/home/roblab20/Desktop/videos/data_oron/data_oron_2023-07-01-22-51-22.csv'
-# filename = '/home/roblab20/Desktop/videos/data_oron/data_oron_2023-07-01-22-50-03.csv'
-filename = '/home/roblab20/Desktop/videos/data_oron/data_oron_2023-06-29-09-57-57.csv'
+filename = '/home/roblab20/Desktop/videos/data_oron/data_oron_2023-07-01-22-50-03.csv'
+# filename = '/home/roblab20/Desktop/videos/data_oron/data_oron_2023-06-29-09-57-57.csv'
 df = pd.read_csv(filename)
 
 
@@ -41,7 +41,6 @@ df = pd.read_csv(filename).dropna()
 M = 14/1000 #kg
 
 
-
 # Given measurements
 pos_x = np.array(df['Pos_x'])
 pos_y = np.array(df['Pos_y'])
@@ -53,20 +52,20 @@ time = np.array(df['Time'])
 phi = np.array(df['Orientation'])
 delta_t = time[1:]- time[:-1]
 
-
+# print('delta t is',delta_t)
 
 # Objective function with L=loss and conditions for the notmal force
 def objective(x):
     f_C, tau_f = x
-
+    #
     eq1= (x_dot[1:] - x_dot[:-1])/delta_t - (x[0]/M)*np.cos(teta)
     eq2 = (y_dot[1:] - y_dot[:-1])/delta_t - (x[0] / M) * np.sin(teta)
     eq3 = (phi_dot[1:]-phi_dot[:-1])/delta_t - (x[0] * ((pos_x[:-1])* np.sin(teta) - (pos_y[:-1]) * np.cos(teta)) + x[1]) / \
           (M * ((pos_x[:-1])**2 + (pos_y[:-1])**2)+ 0.00001)
 
 
-    return  np.sum(np.square(eq3))   # return np.sum(np.square(eq2))
-
+    return  np.sum(np.square(eq3)) + np.sum(np.square(eq1)) + np.sum(np.square(eq2)) # return np.sum(np.square(eq2))
+    # return  np.sum(np.square(eq3))
 # Initial guess for C, K, and L
 x0 = np.array([0.001, 1])
 
@@ -83,9 +82,10 @@ print("Optimized values:")
 print("f_C:", f_c_opt , 'N')
 print("tau_f:", tau_f_opt, 'Nm')
 
+# print(phi_dot[193])
 # check if there is any connection:
-# print((phi_dot[3]) + delta_t[2]*(f_c_opt * ((pos_x[3])* np.sin(teta[3]) - (pos_y[3]) * np.cos(teta[3])) + tau_f_opt) / \
-#           (M * ((pos_x[3])**2 + (pos_y[3])**2)+ 0.00001))
+print((phi_dot[193]) + delta_t[193]*(f_c_opt * ((pos_x[193])* np.sin(teta[193]) - (pos_y[193]) * np.cos(teta[193])) + tau_f_opt) / \
+          (M * ((pos_x[193])**2 + (pos_y[193])**2)+ 0.00001))
 print("##################")
 
 #calculate the error between the path:
@@ -98,9 +98,9 @@ print("##################")
 
 
 def equations_of_motion(x, y, x_dot, y_dot, teta, delta_t, f_c, tau_f ,phi , phi_dot):
-    x_acc = f_c * np.cos(teta)
-    y_acc = f_c * np.sin(teta)
-    phi_acc = (f_c * ((x) * np.sin(teta) - (y) * np.cos(teta))+ tau_f) / (M * ((x)**2 + (y)**2) + 0.0001)
+    x_acc = (f_c/M) * np.cos(teta)
+    y_acc = (f_c/M) * np.sin(teta)
+    phi_acc = (f_c * ((x) * np.sin(teta) - (y) * np.cos(teta))+ tau_f) / (M * ((x)**2 + (y)**2) + 0.00001)
 
     x_dot_new = x_dot + delta_t * x_acc
     y_dot_new = y_dot + delta_t * y_acc
@@ -127,6 +127,7 @@ for i in range(len(delta_t)):
 #
 # print()
 # print(optimized_path_x)
+# print(pos_x.reshape(-1,1))
 # print(optimized_path_y)
 
 # Calculate the errors
@@ -176,7 +177,7 @@ x_margin = 0.005
 y_margin = 0.005
 # x_min, x_max = min(min(pos_x), min(optimized_path_x)) - x_margin, max(max(pos_x), max(optimized_path_x)) + x_margin
 # y_min, y_max = min(min(pos_y), min(optimized_path_y)) - y_margin, max(max(pos_y), max(optimized_path_y)) + y_margin
-plt.xlim(-0.1, 0.05)
+plt.xlim(-0.01, 0.1)
 plt.ylim(-0.1, 0.15)
 #
 plt.grid()
