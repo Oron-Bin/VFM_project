12ad6551be6675210749dc29cb972ad89d205f1b
o-binyamin <oronbinyamin@mail.tau.ac.il>
2023-08-29 17:38:48 +0300

    '29.8 last commit - pivot_code work but not better from second commit !!!!!!

--- Tests/Vision/Aruco/pivot_code.py
+++ Tests/Vision/Aruco/pivot_code.py
@@ -88,10 +88,10 @@ while cam.isOpened():
         # print(aruco_centers)
         if set_des == 0: ## If the user didn't input a value yet
             print("No desired position yet")
-            # algo.y_d = origin[1] ## 220
-            # algo.x_d = origin[0]
-            algo.y_d = 227 ## 220
-            algo.x_d = 642
+            algo.y_d = origin[1] ## 220
+            algo.x_d = origin[0]
+            # algo.y_d = 227 ## 220
+            # algo.x_d = 680
             algo.orientation = random.randint(0,359)
             # algo.x_d = circle_center[0]
             # algo.y_d = circle_center[1]
@@ -130,9 +130,13 @@ while cam.isOpened():
                 cv2.putText(img, f"my_position: {algo.path[-1]}", (10, 90),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-                cv2.putText(img, f"motor_angle: {algo.angle_of_motor()}", (10, 150),
+                # cv2.putText(img, f"motor_angle: {algo.angle_of_motor()}", (10, 150),
+                #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(img, f"desire_angle: {round(algo.orientation,0)}", (10, 150),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(img, f"Time: {time_diff}", (10, 180),
+                # cv2.putText(img, f"Time: {time_diff}", (10, 180),
+                #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(img, f"angle_error: {abs(orientation_angle - algo.orientation)}", (10, 180),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
             # origin = tuple(algo.finger_position(img)) #green point on screen
@@ -154,21 +158,25 @@ while cam.isOpened():
                 # des_orientation = random.randint(0, 359)
                 print('des_orientation', algo.orientation)
                 print('current orientation', orientation_angle)
-                print(abs(orientation_angle - algo.orientation))
-                if abs(orientation_angle-algo.orientation) > 20 :
+                orientation_error = abs(orientation_angle - algo.orientation)
+                print(orientation_error)
+                if orientation_error > 20 :
 
-                    print('condition reached and the error is', abs(orientation_angle-algo.orientation))
+                    print('the error need to fix is', orientation_error)
                     print('**********************************************************')
                     print('**********************************************************')
                     print('**********************************************************')
                     print('**********************************************************')
                     print('**********************************************************')
                     print('**********************************************************')
+
                     if flag ==0:
 
                     # output  = algo.law_1()
                         output = 0
+                        print(output)
                 # delta_list.append(output)
+
                         cv2.putText(img, f"delta_motor_angle: {output}", (10, 120),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                         # cv2.putText(img, f"motor_angle: {algo.angle_of_motor()}", (10, 150),
@@ -180,7 +188,10 @@ while cam.isOpened():
                         mycard.send_data('encoder') ## Send the motor output to the hardware
                         time.sleep(0.001)
                     else:
+                        print('starting navigate to the point')
                         output = algo.law_1()
+                        cv2.putText(img, f"delta_motor_angle: {output}", (10, 120),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                         mycard.set_encoder_angle(output)  ## Update the motor output
                         algo.plot_arrow(img)  ## Plot the direction of the motor
                         mycard.send_data('encoder')  ## Send the motor output to the hardware
@@ -188,7 +199,8 @@ while cam.isOpened():
 
                         if algo.check_distance(10) is True:
                             # # elif algo.check_distance(10) is True and (orientation_angle-des_orientation) <= 2 :
-                            print('Arrived at the goal position', algo.path[-1], 'and orientation')
+                            print('Arrived at the goal position', algo.path[-1], 'and orientation', algo.orientation)
+                            print('the error in the orientation is', orientation_error)
                             for i in range(30):
                                 mycard.send_data('vibrate')
                                 set_des = 3
@@ -198,10 +210,9 @@ while cam.isOpened():
                             time.sleep(3)  # a delay of a second between each iteration
 
                 else:
-                    flag += 1
+                    flag = 1
             # # elif algo.check_distance(epsilon=10) is not True and set_des == 2 and (orientation_angle-des_orientation) <= 2:
-                    print('starting navigate to the point')
-
+                    print('navigate to the point')
                     output = algo.law_1()
                     mycard.set_encoder_angle(output) ## Update the motor output
                     algo.plot_arrow(img) ## Plot the direction of the motor
@@ -210,16 +221,21 @@ while cam.isOpened():
 
                     if algo.check_distance(10) is True:
                 # # elif algo.check_distance(10) is True and (orientation_angle-des_orientation) <= 2 :
-                            print('Arrived at the goal position', algo.path[-1], 'and orientation')
-                            for i in range(30):
-                                mycard.send_data('vibrate')
-                                print('Arrived at the goal pose finalllllllllllllll')
-                                time.sleep(3)
-                                set_des = 3
+                        print('Arrived at the goal position', algo.path[-1], 'and orientation',algo.orientation)
+
+                        for i in range(30):
+                            mycard.send_data('vibrate')
+                            print('Arrived')
+                            print('the orientation_error is', orientation_error)
+                            time.sleep(3)
+                            set_des = 3
 
                     if set_des == 3:
-
                         time.sleep(3)
+
+                        # algo.package_data()
+                        #
+                        # algo.clear()
                         # a delay of a second between each iteration
 
         #             for i in range(30):
