5fd81199638aaa3d6e4b497a022d318642a3a205
o-binyamin <oronbinyamin@mail.tau.ac.il>
2024-07-02 09:42:52 +0300

    '2.7.24 -some change

--- Utils/Other/cam_with_command.py
+++ Utils/Other/cam_with_command.py
@@ -17,7 +17,7 @@ VIDEO_DIR = "/home/roblab20/Desktop/gui_exp"
 VIDEO_FOURCC = cv2.VideoWriter_fourcc(*'XVID')
 
 # Global variables
-# motor_angle_list = [0]
+motor_angle_list = [0]
 
 def command_listener(card, vibration_var, encoder_var, calibrate_btn_var, start_btn_var, stop_btn_var):
     """
