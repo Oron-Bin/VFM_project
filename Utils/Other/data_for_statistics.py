5d977909eac32e2946aa7a009b2151493a7ebf86
o-binyamin <oronbinyamin@mail.tau.ac.il>
2024-10-20 11:17:09 +0300

    '20.10.24 data for statistics'

---
+++ Utils/Other/data_for_statistics.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+import numpy as np
+
+# Define the path to the folder containing the CSV files
+folder_path = '/home/roblab20/Desktop/article_videos/data_full_algo'
+
+# Initialize empty lists to store the errors
+orientation_errors = []
+distance_errors = []
+
+# Iterate through each file in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(folder_path, filename)
+
+        # Load the CSV file
+        df = pd.read_csv(file_path)
+
+        # Get the last value from the 'Orientation Error' and 'Distance Error' columns
+        last_orientation_error = df['Orientation Error'].iloc[-1]
+        last_distance_error = df['Distance Error'].iloc[-1]
+
+        # Append the errors to the respective lists
+        orientation_errors.append(last_orientation_error)
+        distance_errors.append(last_distance_error/2)
+
+# The lists 'orientation_errors' and 'distance_errors' now contain the last values from each file
+# print("Orientation Errors:", orientation_errors)
+# print("Distance Errors:", distance_errors)
+
+orientation_mean = np.mean(orientation_errors)
+distance_mean = np.mean(distance_errors)
+
+print("Orientation Errors:", orientation_mean)
+print("Distance Errors:", distance_mean)
+
